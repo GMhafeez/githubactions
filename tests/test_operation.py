689a4b1d300e5f573266a4664e8732021123047d
unknown <gmhafeez17@gmail.com>
2024-10-01 14:04:34 +0500

    unit test cases

---
+++ tests/test_operation.py
@@ -0,0 +1,9 @@
+from src.math_opereation import add,subtract
+
+def test_Add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+def test_sub():
+    assert subtract(5-3)==2
+    assert subtract(10-10)==0
